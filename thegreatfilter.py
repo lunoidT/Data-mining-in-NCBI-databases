70f3322e2058c78df8c5ca88bee6a2626e754d80
lunoid <lunathorh@gmail.com>
2026-03-23 15:34:49 +0000

    made the first version of preprocessing

---
+++ thegreatfilter.py
@@ -0,0 +1,17 @@
+tax_id = "24"
+
+# Create translation dict for finding relevant gene names
+with open("gene_info") as infile:
+    # geneID_to_name[GeneID] = gene_name
+    geneID_to_name = {}
+    for line in infile:
+        if tax_id == line.split()[0]:
+            geneID_to_name[line.split()[1]] = line.split()[8]
+
+# Create file containing relevant information about organism
+with open("gene2pubmed") as infile, open("processedfile.py", "w") as outfile:
+    outfile.write("#GeneID\tPubMed_ID\tGene_name\n")
+    for line in infile:
+        if tax_id == line.split()[0]:
+            geneID = line.split()[1]
+            outfile.write("\t".join(line.split()[1:]) + "\t" + geneID_to_name[geneID] + "\n")
