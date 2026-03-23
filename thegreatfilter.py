79af67f59ac4beec3c6766adb3158b107c4a28ab
lunoid <lunathorh@gmail.com>
2026-03-23 15:42:13 +0000

    small change

--- thegreatfilter.py
+++ thegreatfilter.py
@@ -6,7 +6,7 @@ with open("gene_info") as infile:
     geneID_to_name = {}
     for line in infile:
         if tax_id == line.split()[0]:
-            geneID_to_name[line.split()[1]] = line.split()[8]
+            geneID_to_name[line.split()[1]] = line.split("\t")[8]
 
 # Create file containing relevant information about organism
 with open("gene2pubmed") as infile, open("processedfile.py", "w") as outfile:
