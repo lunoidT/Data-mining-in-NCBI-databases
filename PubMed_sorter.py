544ba4918039efa9db051b8ce8ce466dc24b863c
lunoid <lunathorh@gmail.com>
2026-04-04 08:20:28 +0000

    made it prettier

--- PubMed_sorter.py
+++ PubMed_sorter.py
@@ -1,20 +1,10 @@
-
+# Files to be used
 processedfile = "processedfile.csv"
 cytofile = "cytoscaper.csv"
+
+
+# ID_dict = {PubmedID, [list of gene names that has this ID]}
 ID_dict = {}
-
-"""with open(processedfile) as infile:
-    for line in infile:
-        PubID = line.split()[1]
-        gene_name = line.split()[2]
-        if PubID not in ID_dict:
-            ID_dict[PubID] = [gene_name,None,0]
-        elif PubID in ID_dict and ID_dict[PubID][1] == None:
-            ID_dict[PubID][1:] = [gene_name,1]
-        else:
-            """
-
-# made a dict that is {PubmedID, [list of gene names that has this ID]}
 with open(processedfile) as infile:
     for line in infile:
         PubID = line.split()[1]
@@ -24,8 +14,8 @@ with open(processedfile) as infile:
         else:
             ID_dict[PubID].append(gene_name)
 
-#print(ID_dict)
-# counting  
+
+# combining different Pubmed IDs and counting their weight
 instance_dict = {} # {combination, weight}
 for PubID,names in ID_dict.items():
     if len(names) > 1:
