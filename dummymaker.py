45d4d2e4291ca2b4acee108a67363477e9c79b6d
mariu <mariusarmangue@gmail.com>
2026-04-05 11:45:33 +0200

    I

--- dummymaker.py
+++ dummymaker.py
@@ -1,6 +1,8 @@
-with open("gene2pubmed") as infile, open("dummy2pubmed", "w") as outfile:
+with open("gene2pubmed") as infile, open("dummy2pubmed_gay", "w") as outfile:
     for line in infile:
         if line.startswith("#"):
             outfile.write(line)
-        if "24" == line.split()[0]:
+        if "9606" == line.split()[0]:
             outfile.write(line)
+        
+
