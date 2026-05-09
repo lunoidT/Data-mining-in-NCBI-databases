4adef101360730da2f02fd127308d5f5046180ec
mariu <mariusarmangue@gmail.com>
2026-05-09 13:28:21 +0200

    I added the first draft to runtime analysis for everything but cytomaker.py

--- src/func/filtering.py
+++ src/func/filtering.py
@@ -1,9 +1,12 @@
+# for runtime analysis, n will indicate genenames and m pubmedID's
+
 def filterwrapper(func):
     def compops(instance_dict:dict, weight:int):
         op = input(f"You've chosen the {str(func).split(' ')[1]}. Please choose one of the following arguments for the filter: less, great, eq, neq\n"
                    "Eg. all values lesser/great than selected weight x or equal/not equal to amount of connections, etc.x\n")
         
-        if op not in ("less", "great", "eq", "neq",):
+        # O(1) due to set
+        if op not in ("less", "great", "eq", "neq"): 
             raise ValueError("The filter isn't filtering due to an insufficient amount of arguments")
 
         # find and return comperative operator for weightfiltering
@@ -16,30 +19,41 @@ def filterwrapper(func):
         elif op == "neq":
             return func(instance_dict, weight, "!=")      
 
+    # The worst case here within the curriculum is O(1)
     return compops
 
+# O(1) from filterwrapper
 @filterwrapper
 def weightfilter(instance_dict:dict, weight:int, op:str):
     """ Selects entries with a specific weight in dictionary.
     Dictionary structure: {(x.y):weight} """
     filtered_dict = {}
 
-    for key in instance_dict:
+    # O(n)
+    for key in instance_dict: 
         if eval(f"{instance_dict[key]} {op} {weight}"):
             filtered_dict[key] = instance_dict[key]
+
+    # Overall runtime O(n+1). Simplified O(n)
     return filtered_dict, op
 
+# O(1) from filterwrapper
 @filterwrapper
 def connectionfilter(pubidnames:dict, min_connections:int, op:str):
     """ Selects entries with a specific amount of connections"""
     from func.namecombiner import combinations
 
     connection_dict = dict()
+    # O(m) (m due to us looping over pubIDs instead of names from the instance_dict)
     for connected_instance in pubidnames:
         if eval(f"{len(pubidnames[connected_instance])} {op} {min_connections}"):
             connection_dict[connected_instance] = pubidnames[connected_instance]
 
-    return combinations(connection_dict), op
+    # O(m*k^2) will be the worst case scnario here. See namecombiner.py for distinctions between cases.
+    combined_dict = combinations(connection_dict)
+
+    # Overall runtime O(m*k^2 + m + 1). Simplified: O(m*k^2)
+    return (combined_dict), op
 
 def namefilter(instancedict:dict, genename:int):
     """ Selects all connections of entries with a specific mentioned gene-name"""
@@ -48,30 +62,35 @@ def namefilter(instancedict:dict, genename:int):
     op = input("You've selected namefilter. Please choose one of the following arguments: including, excluding\n"
         "E.g. all entries including/excluding this genename\n")
 
+    # O(1)
     if op not in ("including","excluding"):
             raise ValueError("The filter isn't filtering due to an insufficient amount of arguments")
 
     namefitereddict = dict()
     if op == "including":
+        # O(n)
         for instance in instancedict:
             if genename in instance:
                 namefitereddict[instance] = instancedict[instance]
     elif op == "excluding":
+        # O(n)
         for instance in instancedict:
-            for instance in instancedict:
-                if genename not in instance:
-                    namefitereddict[instance] = instancedict[instance]
+            if genename not in instance:
+                namefitereddict[instance] = instancedict[instance]
             
     if instancedict and not namefitereddict:
         print(f"Warning! The file is now empty due to your filtering preferences. It wasn't before!")    
     
+    # Overall runtime O(2n+1). After simplifying: O(n)
     return namefitereddict, op
 
+# O(1) from filterwrapper
 @filterwrapper
 def sumofconnectionfilter(instancedict:dict, targetsum:int, op:str):
     """ Computes the weighed sum of connections and filters accordingly"""
     connectiondict = dict()
 
+    # O(n)
     for connected_instance in instancedict:
         if (connected_instance[0] in connectiondict):
             connectiondict[connected_instance[0]] += int(instancedict[connected_instance])
@@ -82,14 +101,12 @@ def sumofconnectionfilter(instancedict:dict, targetsum:int, op:str):
         else:
             connectiondict[connected_instance[1]] = int(instancedict[connected_instance])
 
+    # O(n)
     for key in list(instancedict.keys()):
-        print(key, "\t", instancedict[key])
-
         if not eval(f"{connectiondict[key[0]]} {op} {targetsum}"):
             del instancedict[key]
         elif not eval(f"{connectiondict[key[1]]} {op} {targetsum}"):
             del instancedict[key]
 
-    print(instancedict)
-
+    # Overall runtime O(2n + 1). After simplifying: O(n)
     return instancedict, op
