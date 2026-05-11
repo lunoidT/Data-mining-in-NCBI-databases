d509c0f9d032ddd4a9851c10de863d86229b7828
mariu <mariusarmangue@gmail.com>
2026-05-11 16:12:55 +0200

    Deleted outdated/unnecessary files

--- dummymaker.py
+++
@@ -1,8 +0,0 @@
-with open("gene2pubmed") as infile, open("dummy2pubmed_gay", "w") as outfile:
-    for line in infile:
-        if line.startswith("#"):
-            outfile.write(line)
-        if "9606" == line.split()[0]:
-            outfile.write(line)
-        
-
--- dummymaker_info.py
+++
@@ -1,4 +0,0 @@
-with open("gene_info") as infile, open("smalldummy_info", "w") as outfile:
-    for line in infile:
-        if line.startswith("24"):
-            outfile.write(line)
