6729f982d6a3103b180fdfad20ae56b73b989bca
mariu <mariusarmangue@gmail.com>
2026-05-11 16:17:01 +0200

    Deleted outdated testfile as well

--- testdata/geneinfo_mk.py
+++
@@ -1,8 +0,0 @@
-with open("test2pubmed") as infile, open("testgene_info") as infile2, open("newtestgene_info","w") as outfile:
-    nameset = set()
-    for line in infile:
-        nameset.add(line.split()[1])
-
-    for line in infile2:
-        if line.split()[1] in nameset:
-            outfile.write(line)
